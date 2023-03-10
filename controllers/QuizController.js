0a591f93d96ccbcf2133bc7cb4c4de8d7a9a1834
besufikad17 <besufikadtilahun1994@gmail.com>
2023-03-11 00:36:38 +0300

    DB and prisma updated to mongodb

--- controllers/QuizController.js
+++ controllers/QuizController.js
@@ -15,35 +15,33 @@ quizController.addQuiz = async(req, res) => {
     }
 
     try {
-        const newQuiz = await prisma.quiz.create({
-            data:{
-                number_of_questions: questions.length,
-                views: 0,
-                tag,
-                created_at: new Date(),
-            }
-        })
 
-        let qa = [];
+       let qa = [];
 
-        for(let i = 0; i < questions.length; i++){
-            qa.push({
-                content: questions[i],
-                language: language,
-                choices: choices[i],
-                answer: answers[i]
-            })  
+       for(let i = 0; i < questions.length; i++){
+         qa.push({
+            content: questions[i],
+            choices: choices[i],
+            answer: answers[i]
+         })
+       }
+
+       const newQuiz = await prisma.quiz.create({
+        data: {
+            number_of_questions: questions.length,
+            views: 0,
+            tag,
+            language,
+            created_at: new Date(),
+            questions: qa
         }
+       })
 
-        res.json({
-            success: true,
-            data: {
-                ...newQuiz,
-                views: newQuiz.views.toString(),
-                qa
-            },
-            error: null
-        })
+       res.json({
+        success: true,
+        data: {...newQuiz, views: newQuiz.views.toString() },
+        error: null
+       })
         
     } catch (error) {
         console.log(error);
@@ -56,7 +54,8 @@ quizController.addQuiz = async(req, res) => {
 }
 
 quizController.getQuiz = async(req, res) => {
-    const 
+    const telegram_id = req.params.telegram_id;
+    console.log(telegram_id);
 }
 
 module.exports = quizController;
